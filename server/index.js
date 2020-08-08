f947871a6df9cdae1dac83f0789810024e820434
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-08-07 23:31:16 -0700

    Configure webpack/seeder script

---
+++ server/index.js
@@ -0,0 +1,42 @@
+// install dependencies
+const express = require('express');
+const app = express();
+const path = require('path');
+const db = require('../database/index.js');
+
+const PORT = 3000;
+
+// create middleware
+let logRequests = (req, res, next) => {
+  console.log(`Request type: ${req.method} from path: ${req.path}`);
+  next();
+}
+app.use(logRequests);
+app.use(express.static(path.join(__dirname, './client/dist')))
+
+
+// create routes
+app.get('/test-seeder', (req, res) => {
+  db.getAllProperties( (err, results) => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(results)
+      res.status(200).send(results);
+    }
+  })
+})
+
+app.post('/test-seeder', (req, res) => {
+  db.seeder();
+  res.status(201).send();
+})
+
+// start server
+app.listen(PORT, (err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Server is listening on port:', PORT);
+  }
+})
