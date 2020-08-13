29d0f68d75a03bf5dea34e36322905813cd80e6e
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-08-12 22:06:32 -0700

    Wrote some tests

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,25 +6,26 @@ import Button from '../styleComponents/Button.js';
 import Wrapper from '../styleComponents/Wrapper.js';
 
 let query = window.location.search;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       properties: [],
-      property: null,
-      relatedProperties: []
+      // property: null,
+      // relatedProperties: []
     },
 
     this.setProperties = this.setProperties.bind(this);
     this.getProperties = this.getProperties.bind(this);
-    this.setProperty = this.setProperty.bind(this);
-    this.getProperty = this.getProperty.bind(this);
-    this.setRelatedProperties = this.setRelatedProperties.bind(this);
+    // this.setProperty = this.setProperty.bind(this);
+    // this.getProperty = this.getProperty.bind(this);
+    // this.setRelatedProperties = this.setRelatedProperties.bind(this);
   }
 
   componentDidMount() {
     this.getProperties();
-    this.getProperty();
+    // this.getProperty();
   }
 
   getProperties() {
@@ -35,41 +36,38 @@ class App extends React.Component {
 
   setProperties(properties) {
     this.setState({ properties: properties.data });
-    console.log(this.state.properties);
   }
 
-  getProperty() {
-    axios.get(`/test1/${query}`)
-      .then( (result) => {
-        this.setProperty(result);
-        return this.state.property;
-      })
-      .then(this.setRelatedProperties)
-      .catch(console.log);
-  }
+  // getProperty() {
+  //   axios.get(`/test1/${query}`)
+  //     .then( (result) => {
+  //       this.setProperty(result);
+  //       return this.state.property;
+  //     })
+  //     // .then(this.setRelatedProperties)
+  //     .catch(err);
+  // }
 
-  setProperty(property) {
-    this.setState({ property: property.data[0] });
-    console.log(property.data[0])
-  }
+  // setProperty(property) {
+  //   this.setState({ property: property.data[0] });
+  // }
 
-  setRelatedProperties() {
-    for (let i = 0; i < this.state.property.similarPlaces.length; i += 1) {
-      let similarPlace = this.state.property.similarPlaces[i]
-      for (let j = 0; j < this.state.properties.length; j += 1) {
-        let property = this.state.properties[i];
-        if (similarPlace === property.propertyId) {
-          this.setState({ relatedProperties: [...this.state.relatedProperties, property]});
-        }
-      }
-    }
+  // setRelatedProperties() {
+  //   for (let i = 0; i < this.state.property.similarPlaces.length; i += 1) {
+  //     let similarPlace = this.state.property.similarPlaces[i]
+  //     for (let j = 0; j < this.state.properties.length; j += 1) {
+  //       let property = this.state.properties[i];
+  //       if (similarPlace === property.propertyId) {
+  //         this.setState({ relatedProperties: [...this.state.relatedProperties, property]});
+  //       }
+  //     }
+  //   }
 
-  }
+  // }
 
   render() {
     return (
       <Wrapper>
-      {console.log(this.state.relatedProperties)}
         <MorePlaces places={this.state.properties} />
         <Button>Click</Button>
       </Wrapper>
--- client/src/styleComponents/Wrapper.js
+++ client/src/styleComponents/Wrapper.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
 export default styled.div`
-  flex-direction: row
+  display: flex;
+  flex-direction: row;
 `;
