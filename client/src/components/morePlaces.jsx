2ff943a6d9b59446c45c1c387e7a539c1e470465
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-08-14 20:31:04 -0700

    make basic buttons for test

--- client/src/components/morePlaces.jsx
+++ client/src/components/morePlaces.jsx
@@ -8,34 +8,65 @@ class MorePlaces extends React.Component {
     this.state = {
       pageNum: 0
     }
+    this.nextArrowClick = this.nextArrowClick.bind(this);
+    this.prevArrowClick = this.prevArrowClick.bind(this);
   }
 
   nextArrowClick() {
-    this.setState({ pageNum: (pageNum + 1) % 3 });
+    this.setState({ pageNum: (this.state.pageNum + 1) % 3 });
   }
 
   prevArrowClick() {
-    this.setState({ pageNum: (pageNum - 1) % 3 });
+    this.setState({ pageNum: (this.state.pageNum - 1) % 3 });
     if (this.state.pageNum < 0) {
-      this.setState({ pageNum: (pageNum + 3) });
+      this.setState({ pageNum: (this.state.pageNum + 3) });
     }
   }
 
   render() {
 
     return (
+      <div>
+        <NavBar><PrevButton onClick={this.prevArrowClick}>prev</PrevButton><NextButton onClick={this.nextArrowClick}>next</NextButton></NavBar>
       <Wrapper>
+
         {this.props.places.map( (place) =>
           <MorePlacesEntry key={place.propertyId} place={place} />
         )}
       </Wrapper>
+      </div>
     )
   }
 }
 
+const NavBar = styled.div`
+  display: inline;
+  float: right;
+  padding: 5px 0 5px 0;
+`;
+
+const PrevButton = styled.button`
+  border-radus: 50%;
+  border: 1px solid #C5C5C5;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  font-weight: 400;
+  font-size: 16px;
+  margin-right: 5px;
+`;
+
+const NextButton = styled.button`
+  border-radus: 50%;
+  border: 1px solid #C5C5C5;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  font-weight: 400;
+  font-size: 16px;
+  margin-left: 5px;
+`;
+
 const Wrapper = styled.div`
 display: flex;
 flex-direction: row;
+clear: both;
 `;
 
 export default MorePlaces;
