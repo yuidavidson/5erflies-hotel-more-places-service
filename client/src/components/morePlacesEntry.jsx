96e92617a950cb0fc9e18d7d31bcfb8ec8a9cd31
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-08-17 11:58:08 -0700

    Changed morePlacesEntry to be stateful to be able to render superhost

--- client/src/components/morePlacesEntry.jsx
+++ client/src/components/morePlacesEntry.jsx
@@ -1,20 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
 
-let MorePlacesEntry = (props) => (
-  <Card>
-    <ImageContainer>
-      <HeartImg src={heartImg} />
-      <a href={`?propertyId=${props.place.propertyId}`}><Image src={props.place.img} /></a>
-    </ImageContainer>
-    <div className="property-description">
-      <div>{props.place.propertyType} • {props.place.numOfRooms} beds</div>
-      <div><StarImg src={starImg} /> {props.place.rating} <NumRatings>({props.place.numOfRatings})</NumRatings></div>
-      <div>{props.place.description}</div>
-      <div><Price>${props.place.price}</Price> / night</div>
-    </div>
-  </Card>
-)
+// let MorePlacesEntry = (props) => (
+  // <Card>
+  // {if(props.place.isSuperHost) <SuperHost></SuperHost>}
+  //   <ImageContainer>
+  //     <HeartImg src={heartImg} />
+  //     <a href={`?propertyId=${props.place.propertyId}`}><Image src={props.place.img} /></a>
+  //   </ImageContainer>
+  //   <div className="property-description">
+  //     <div>{props.place.propertyType} • {props.place.numOfRooms} beds</div>
+  //     <div><StarImg src={starImg} /> {props.place.rating} <NumRatings>({props.place.numOfRatings})</NumRatings></div>
+  //     <div>{props.place.description}</div>
+  //     <div><Price>${props.place.price}</Price> / night</div>
+  //   </div>
+  // </Card>
+// )
+class MorePlacesEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    if (this.props.place.isSuperHost) {
+      return (
+        <Card>
+          <ImageContainer>
+            <SuperHost><SuperHostCon>SUPERHOST</SuperHostCon></SuperHost>
+            <HeartImg src={heartImg} />
+            <a href={`?propertyId=${this.props.place.propertyId}`}><Image src={this.props.place.img} /></a>
+          </ImageContainer>
+          <div className="property-description">
+            <div>{this.props.place.propertyType} • {this.props.place.numOfRooms} beds</div>
+            <div><StarImg src={starImg} /> {this.props.place.rating} <NumRatings>({this.props.place.numOfRatings})</NumRatings></div>
+            <div>{this.props.place.description}</div>
+            <div><Price>${this.props.place.price}</Price> / night</div>
+          </div>
+        </Card>
+      )
+    } else {
+      return (
+        <Card>
+          <ImageContainer>
+            <HeartImg src={heartImg} />
+            <a href={`?propertyId=${this.props.place.propertyId}`}><Image src={this.props.place.img} /></a>
+          </ImageContainer>
+          <div className="property-description">
+            <div>{this.props.place.propertyType} • {this.props.place.numOfRooms} beds</div>
+            <div><StarImg src={starImg} /> {this.props.place.rating} <NumRatings>({this.props.place.numOfRatings})</NumRatings></div>
+            <div>{this.props.place.description}</div>
+            <div><Price>${this.props.place.price}</Price> / night</div>
+          </div>
+        </Card>
+      )
+    }
+  }
+}
 let starImg = "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-14+at+9.41.44+PM.png";
 let heartImg = "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/airbnb-heart.png"
 
@@ -45,6 +89,24 @@ const Image = styled.img`
   position: relative;
   z-index: 0;
 `;
+
+const SuperHost = styled.div`
+  position: absolute;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif !important;
+  background-color: rgba(255, 255, 255, 0.95);
+  border: 0.5px solid rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  margin: 5px;
+  color: rgb(34, 34, 34);
+  z-index: 1;
+`;
+
+const SuperHostCon = styled.span`
+  margin: 10px;
+  font-weight: bold;
+  font-size: 14px;
+`
+
 const HeartImg = styled.img`
   height: 14px;
   width: 16px;
