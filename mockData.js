515f2544f39b5ab589f39fb1792cae82a421e9bf
Tom Nguyen <tommy.nguyen0721@gmail.com>
2020-08-18 10:07:39 -0700

    Change where mockData is located

---
+++ mockData.js
@@ -0,0 +1,60 @@
+module.exports = () => {
+ let propertyImg = [
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+4.00.14+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+4.51.26+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+4.51.55+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+4.52.05+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.04+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.11+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.16+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.21+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.24+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.24+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.27+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.30+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.32+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.35+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.38+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.41+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.16.44+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.31.52+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.31.59+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.32.03+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.32.06+PM.png",
+    "https://hackreactor5erfliesmoreplaces.s3-us-west-1.amazonaws.com/Screen+Shot+2020-08-07+at+6.32.09+PM.png"
+  ]
+
+  let propertyDescription = [
+    "Beautiful room overlooking city",
+    "Great place for family vacation",
+    "Little summer get away home",
+    "Private place to relax",
+    "Historic house in center of city",
+    "Great location in middle of attractions",
+    "Lovely place for relaxation",
+    "Luxury hide-away",
+    "Hakuna Matata - close to downtown",
+    "Professionally designed place!",
+    "Nantucket Style Beach House",
+    "Very large backyard with grill and swings!",
+    "House w/big yard!",
+    "Pet friendly bungalow",
+    "Entire Guesthouse fits plenty",
+    "Lots of food and drinks in fridge",
+    "How many of these will i write?",
+    "Country Cabin in the city",
+    "Great location with great views",
+    "Don't stay here!",
+    "Kanto Region",
+    "Hot Spring Onsen",
+    "Best place to stay at in the world!",
+    "Need to sleep? Heres the place"
+  ]
+
+  let propertyTypes = [
+    "Entire Place",
+    "Private Room",
+    "Hotel Room",
+    "Shared Room"
+  ]
+}
